3aeae7ebc80133e9843a826a51f57263fad630dc
Alikhan <kithen59@gmail.com>
2024-01-16 20:53:58 +0600

    Second commit

--- labs 1/strings.py
+++ labs 1/strings.py
@@ -5,3 +5,40 @@ print(len(x))
 
 # ex 2
 
+txt = "Hello World"
+x = txt[0]
+
+# ex 3
+
+txt = "Hello World"
+x = txt[2:5]
+
+# ex 4
+
+txt = " Hello World "
+x = txt.strip()
+
+# ex 5
+
+txt = "Hello World"
+txt = txt.upper()
+
+# ex 6
+
+txt = "Hello World"
+txt = txt.lower()
+
+# ex 7
+
+txt = "Hello World"
+txt = txt.replace("H", "J")
+
+# ex 8
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
+
+
